7ac59a6950e8702924400e8b1b1e85f2a21d31cb
Winston Sevilla <winston_sevilla@outlook.com>
2021-10-31 17:41:05 +0800

    Basket.API. configure BasketController.

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -1,15 +1,10 @@
+using Basket.API.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Basket.API
 {
@@ -28,6 +23,9 @@ namespace Basket.API
             services.AddStackExchangeRedisCache(options => {
                 options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
             });
+
+            services.AddScoped<IBasketRepository, BasketRepository>();
+
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
